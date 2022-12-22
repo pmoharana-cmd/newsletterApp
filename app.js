0f58f720bfebcfe23bd3e602b75a2c7b39d03603
pmoharana-cmd <78167757+pmoharana-cmd@users.noreply.github.com>
2022-12-22 15:43:06 -0500

    Add comments on app.js

--- app.js
+++ app.js
@@ -4,27 +4,34 @@ import path from "path";
 import { fileURLToPath } from "url";
 import client from "@mailchimp/mailchimp_marketing";
 
+// Intialize express app
 const app = express();
+
+// Set config for MailChimp API using env variables
 client.setConfig({
+  // variables set in Cyclic dashboard
   apiKey: process.env.API_KEY,
   server: process.env.SERVER_LOC,
 });
 
+// Create a static folder for relative paths when using images, css, and javascript
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Declare __filename and __dirname
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Newsletter Page
-
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "signup.html"));
 });
 
+// Post request when submit button is clicked on Sign-Up Page
 app.post("/", (req, res) => {
   var subscribingUser = req.body;
 
+  // Data is collected into a javascript object and is passed addListMember()
   const data = {
     email_address: subscribingUser.email,
     status: "subscribed",
@@ -36,6 +43,7 @@ app.post("/", (req, res) => {
   };
 
   const run = async () => {
+    // Loop is used to determine success or failure
     try {
       const response = await client.lists.addListMember(
         process.env.LIST_ID,
@@ -47,6 +55,7 @@ app.post("/", (req, res) => {
     }
   };
 
+  // Runs the function that adds the member to the mailing list
   run();
 });
 
@@ -56,7 +65,6 @@ app.post("/failure", (req, res) => {
 });
 
 // Start Up Server
-
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server has been started on port 3000.");
 });
